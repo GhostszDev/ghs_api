9b95563cfcd23f8efd393e0d9471c4a057fa37b5
Steven Rivera <ghostszmusic@gmail.com>
2018-07-14 10:41:11 -0500

    removed

--- libs/ghs_includes.php
+++ libs/ghs_includes.php
@@ -1,17 +1,2 @@
 <?php
 
-// const vars
-const CLIENT_ID     = 'ccfHO00GfOew8TWAWYLXLH4Gmojj6rEq5d8wXJmp';
-const CLIENT_SECRET = 'xA55vWTye85aSVbsy1zZuZH1BOUi6Nr7F13StXEu';
-
-const REDIRECT_URI           = 'https://ghostszmusic.com/appData';
-const AUTHORIZATION_ENDPOINT = 'https://ghostszmusic.com/oauth/authorize';
-const TOKEN_ENDPOINT         = 'https://ghostszmusic.com/oauth/token';
-
-const G_KEY = 'AIzaSyBPRQK0VSndolG08-S76iz18viv2tAXF6I';
-const FB_ID = '220252511670899';
-const FB_SC = '8dcca404ee71c5ca451866456c9d411c';
-const FB_Version = 'v2.12';
-const Insta_ID = '8031be64fc90427e97bc83939cd37057';
-const Insta_SC = 'afd8865f3b244c53b2c85c813e6ff179';
-const YT_ID = 'UCVmQ1mT50ksSLUK1KEFJ67w';
